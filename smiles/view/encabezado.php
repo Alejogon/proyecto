b968e068275063136dd663e96a67f0d78c35a002
alejogon <alejandro@kenssey.com>
2025-04-16 20:19:31 -0500

    rutas absolutas

--- smiles/view/encabezado.php
+++ smiles/view/encabezado.php
@@ -1,7 +1,7 @@
  <!-- place navbar here -->
  <nav class="navbar navbar-expand-lg navbar-light bg-light">
                 <div class="container">
-                    <a class="navbar-brand" href="../inicio"><img src="img/logoDental.png" alt="Smiles" class="d-inline-block align-text-center"width="200px" height="150px"></a>
+                    <a class="navbar-brand" href="../inicio"><img src="../img/logoDental.png" alt="Smiles" class="d-inline-block align-text-center"width="200px" height="150px"></a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" 
                             data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
@@ -9,18 +9,18 @@
                     <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
                     <ul class="navbar-nav">
                     <li class="nav-item">
-                            <a type="button" class="btn btn-outline-success me-2" href='view/view_agendar_especialidad'>Consultar Agenda por Especialista</a>
+                            <a type="button" class="btn btn-outline-success me-2" href='/smiles/view/view_agendar_especialidad'>Consultar Agenda por Especialista</a>
                         </li>
                         <li class="nav-item">
-                            <a type="button" class="btn btn-outline-success me-2" href='view/view_agendar'>Consultar agenda por fecha</a>
+                            <a type="button" class="btn btn-outline-success me-2" href='/smilesview/view_agendar'>Consultar agenda por fecha</a>
                         </li>
                         <li class="nav-item">
-                            <a type="button" class="btn btn-outline-success me-2" href='view/view_cancelar_cita'>Cancelar cita</a>
+                            <a type="button" class="btn btn-outline-success me-2" href='/smilesview/view_cancelar_cita'>Cancelar cita</a>
                         </li>
                         <li class="nav-item">
-                            <a type="button" class="btn btn-outline-success me-2" href='view/view_consultar_historia'>Consultar Historia Médica</a>
+                            <a type="button" class="btn btn-outline-success me-2" href='/smilesview/view_consultar_historia'>Consultar Historia Médica</a>
                         <li class="nav-item">
-                            <a type="button" class="btn btn-primary" href='controler/controlador_cerrar_sesion'>Salir</a>
+                            <a type="button" class="btn btn-primary" href='/smiles/controler/controlador_cerrar_sesion'>Salir</a>
                         </li>
                     </ul>
                     </div>
